a6e4fe2af9984dba4adf0f2c3cd7f0728cde5b1d
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2024-12-04 15:40:21 -0500

    add storybook

---
+++ apps/storybook/src/orbit-parallax.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import "@bluecadet/orbit-vanilla/orbit-parallax";
+
+const meta: Meta = {
+  component: 'orbit-parallax',
+};
+
+export default meta;
+type Story = StoryObj;
+
+export const Primary: Story = {
+  args: {
+    
+  },
+};
