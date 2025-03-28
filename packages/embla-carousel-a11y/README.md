a638093494621df24f5f31cb359a5c22e9cfc832
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-03-28 10:42:49 -0400

    initialize embla a11y package

---
+++ packages/embla-carousel-a11y/README.md
@@ -0,0 +1,3 @@
+# Embla Carousel A11y
+
+⚠️ This package is in early development and is not yet ready for production use. ⚠️
---
+++ packages/embla-carousel-a11y/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+  entryPoints: [
+    "src/index.ts",
+  ],
+  format: ["cjs", "esm"],
+  dts: true,
+  sourcemap: false,
+  external: [],
+  ...options,
+}));
