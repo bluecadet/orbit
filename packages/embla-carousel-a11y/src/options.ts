977a09a442ddc85c1da83bd963527f7b5b4d0540
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-03-28 11:24:05 -0400

    add embla plugin structure

---
+++ packages/embla-carousel-a11y/src/options.ts
@@ -0,0 +1,11 @@
+import { CreateOptionsType } from "embla-carousel";
+
+export type OptionsType = CreateOptionsType<{
+  // TODO: add options here
+}>;
+
+export const defaultOptions: OptionsType = {
+  active: true,
+  breakpoints: {},
+  // TODO: add default options here
+};
