818bbe796cc28dd18ccc9f3a0af650f0abf5e6b1
Clay Tercek <30105080+claytercek@users.noreply.github.com>
2025-04-14 15:49:48 -0400

    rewrite carousel with embla args and default args

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,10 +1,25 @@
 import type { Preview } from "@storybook/web-components";
-import customElements from "@bluecadet/orbit-vanilla/custom-elements.json";
+import customElements from "@bluecadet/orbit-vanilla/custom-elements.json" assert { type: "json" };
 import { setCustomElementsManifest } from "@storybook/web-components";
 
 import "./styles.css";
 
-setCustomElementsManifest(customElements);
+// strip members from modules, as storybook displays them in the controls,
+// and in most cases it's not useful to have them there.
+const modifiedElementManifest = {
+  ...customElements,
+  modules: customElements.modules.map((module) => ({
+    ...module,
+    declarations: module.declarations.map((declaration) => ({
+      ...declaration,
+      // TBD, we might want to be more selective, rather than just
+      // hiding EVERY member
+      members: [],
+    })),
+  })),
+};
+
+setCustomElementsManifest(modifiedElementManifest);
 
 const preview: Preview = {
   parameters: {
